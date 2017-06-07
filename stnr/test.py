b6e105bdfddf527820ef6c37322715dec943e0cb
Nikita Endaltsev <serafim-end@yandex.ru>
2017-06-07 18:08:59 +0300

    unnes changes

--- stnr/test.py
+++ stnr/test.py
@@ -3,13 +3,17 @@
 from suffix_tree import SuffTree
 from algorithm import STNRAlgorithm
 
+
 def test():
 
     STNRAlgorithm.s = 'abcdeabcdeabcdeabcdeabcdeabcdeabcde' + '$'
     print '01234567890123456789'
     print
 
+    stnr_instance = STNRAlgorithm()
+
     st = SuffTree(
+        stnr_instance=stnr_instance,
         T=STNRAlgorithm.s,
         minThreshold=STNRAlgorithm.minTh,
         tolWin=STNRAlgorithm.tolWin,
